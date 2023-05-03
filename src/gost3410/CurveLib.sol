ac3239c5f9d1b0895666c28068021b9122583c0b
Eduard Iskandarov <eduard.iskandarov@yandex.ru>
2023-05-03 12:54:12 +0500

    ✨ implement verify function

--- src/gost3410/CurveLib.sol
+++ src/gost3410/CurveLib.sol
@@ -98,12 +98,17 @@ library CurveLib {
 
     function exp(
         Curve memory curve,
+        uint degree
+    ) internal pure returns (uint, uint) {
+        return exp(curve, degree, curve.x, curve.y);
+    }
+
+    function exp(
+        Curve memory curve,
         uint degree,
         uint x,
         uint y
     ) internal pure returns (uint, uint) {
-        x = x != 0 ? x : curve.x;
-        y = y != 0 ? y : curve.y;
         uint tx_ = x;
         uint ty = y;
 
@@ -117,5 +122,69 @@ library CurveLib {
             (x, y) = _add(curve, x, y, x, y);
         }
         return (tx_, ty);
+    }
+
+    /**
+     * Verify provided digest with the signature
+     *
+     * @param {Curve} curve - curve to use
+     * @param {uint} pubX - x component of public key
+     * @param {uint} pubY - y component of public key
+     * @param {bytes32} digest - digest needed to check
+     * @param {64 bytes} signature - signature to verify with
+     * @return {bool}
+     */
+    function verify(
+        Curve memory curve,
+        uint pubX,
+        uint pubY,
+        bytes32 digest,
+        bytes memory signature
+    ) internal pure returns (bool) {
+        require(signature.length == 64, "Invalid signature length");
+
+        uint q = curve.q;
+        uint p = curve.p;
+
+        (uint r, uint s) = parseSignature(signature);
+
+        if (r <= 0 || r >= q || s <= 0 || s >= q) {
+            return false;
+        }
+        uint e = uint(digest) % curve.q;
+        if (e == 0) {
+            e = 1;
+        }
+
+        uint v = e.modinv(q);
+        uint z1 = (s * v) % q;
+        uint z2 = q - ((r * v) % q);
+        (uint p1x, uint p1y) = exp(curve, z1);
+        (uint q1x, uint q1y) = exp(curve, z2, pubX, pubY);
+        uint lm = q1x - p1x;
+        if (lm < 0) {
+            lm += p;
+        }
+        lm = lm.modinv(p);
+        z1 = q1y - p1y;
+        lm = (lm * z1) % p;
+        lm = (lm * lm) % p;
+        lm = lm - p1x - q1x;
+        lm = lm % p;
+        if (lm < 0) {
+            lm += p;
+        }
+        lm %= q;
+
+        return lm == r;
+    }
+
+    function parseSignature(
+        bytes memory signature
+    ) internal pure returns (uint r, uint s) {
+        assembly {
+            r := mload(add(signature, 32))
+            s := mload(add(signature, 64))
+        }
     }
 }
